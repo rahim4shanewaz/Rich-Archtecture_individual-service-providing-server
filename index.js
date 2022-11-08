32bd17d4dc9c764428ead4cb15b14d72eebd3900
rahim4shanewaz <soumiq444@gmail.com>
2022-11-08 14:47:18 +0600

    mongoDB connected

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+
+require('dotenv').config();
+const app = express();
+
+
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+// console.log(process.env.DB_USER)
+// console.log(process.env.DB_PASS)
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.upngnl9.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+    try{
+
+        const serviceCollection = client.db('g-car').collection('services');
+        // const orderCollection =client.db('g-car').collection('orders');
+
+
+        app.get('/services', async (req, res) =>{
+            const query ={};
+            const cursor =  serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+
+
+    }
+    finally{
+
+    }
+}
+
+run().catch(err => console.log(err));
+
+
+
+app.get('/', (req, res)=>{
+    res.send('Assignment server Running');
+});
+
+
+app.listen(port, () => {
+    console.log(`running on this port ${port}`)
+    })
