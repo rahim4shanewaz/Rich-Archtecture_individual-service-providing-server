9f6181ab542ea9658d0a219970c8d368fd787ac0
rahim4shanewaz <soumiq444@gmail.com>
2022-11-10 21:09:03 +0600

    some change

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
@@ -33,7 +34,7 @@ function verifyJWT(req, res, next){
     }
     const token = authHeader.split(' ')[1];
 
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
+    jwt.verify(token, process.env.TOKEN_SC, function(err, decoded){
         if(err){
             return res.status(403).send({message: 'Forbidden access'});
         }
@@ -61,7 +62,8 @@ async function run(){
 
         app.post('/jwt', (req, res) =>{
             const user = req.body;
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d'})
+            console.log(user);
+            const token = jwt.sign(user, process.env.TOKEN_SC, { expiresIn: '1d'})
             res.send({token})
         })  
 
@@ -75,7 +77,7 @@ async function run(){
         app.get('/services', async (req, res) =>{
            
             const query ={};
-            const cursor =  serviceCollection.find(query).limit(3);
+            const cursor =  serviceCollection.find(query).limit(3).sort({timestamp: -1});
             const services = await cursor.toArray();
             res.send(services);
         });
@@ -84,16 +86,16 @@ async function run(){
         app.get('/allservices', async (req, res) =>{
            
             const query ={};
-            const cursor =  serviceCollection.find(query);
+            const cursor =  serviceCollection.find(query).sort({timestamp: -1});
             const services = await cursor.toArray();
             res.send(services);
         });
 
-        app.post('/addservice', async (req, res) => {
-            const service = req.body;
-            const result = await serviceCollection.insertOne(service);
-            res.send(result);
-        });
+        // app.post('/addservice', async (req, res) => {
+        //     const service = req.body;
+        //     const result = await serviceCollection.insertOne(service);
+        //     res.send(result);
+        // });
 
      
         
@@ -107,7 +109,20 @@ async function run(){
           });
 
 
-        app.get('/userServices', async (req, res) => {
+          app.get('/productreviews/id', async (req, res) => {
+
+
+            const id = req.params.id;
+
+            let query = {_id: ObjectId(id)};
+           
+            const cursor = ReviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        });
+
+
+        app.get('/userServices', verifyJWT, async (req, res) => {
 
             const decoded = req.decoded;
             
@@ -115,6 +130,7 @@ async function run(){
                 res.status(403).send({message: 'unauthorized access'})
             }
 
+            
             let query = {};
             if (req.query.email) {
                 query = {
@@ -124,6 +140,95 @@ async function run(){
             const cursor = serviceCollection.find(query);
             const Services = await cursor.toArray();
             res.send(Services);
+        });
+
+
+        app.get('/userreviews', verifyJWT, async (req, res) => {
+
+            const decoded = req.decoded;
+            
+            if(decoded.email !== req.query.email){
+                res.status(403).send({message: 'unauthorized access'})
+            }
+            let query = {};
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = ReviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        });
+
+        app.get('/productreviews', async (req, res) => {
+
+            
+            let query = {};
+            if (req.query.id) {
+                query = {
+                    id: req.query.id
+                }
+            }
+            const cursor = ReviewCollection.find(query).sort({timestamp: -1});
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        });
+        
+        app.delete('/review/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await ReviewCollection.deleteOne(query);
+            res.send(result);
+        })
+        
+        app.delete('/service/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await serviceCollection.deleteOne(query);
+            res.send(result);
+        })
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // app.get('/productreviews/id', async (req, res) => {
+
+        //     const id = req.params.id;
+
+
+        //     let query = {};
+        //     if (req.query.id) {
+        //         query = {
+        //             id: req.query.id
+        //         }
+        //     }
+        //     const cursor = ReviewCollection.find(query);
+        //     const reviews = await cursor.toArray();
+        //     res.send(reviews);
+        // });
+
+
+      
+
+
+
+
+
+
+
+
+
+
+        app.post('/addservice', verifyJWT, async (req, res) => {
+            const Services = req.body;
+            const result = await serviceCollection.insertOne(Services);
+            res.send(result);
         });
 
 
@@ -140,19 +245,7 @@ async function run(){
 
 
 
-        app.get('/userreviews', async (req, res) => {
-            
-
-            let query = {};
-            if (req.query.email) {
-                query = {
-                    email: req.query.email
-                }
-            }
-            const cursor = serviceCollection.find(query);
-            const reviews = await cursor.toArray();
-            res.send(reviews);
-        });
+     
 
        
 
